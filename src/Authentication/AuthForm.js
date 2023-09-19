eaf79b7f24eb88d90450a3bfd416f51a880f3d5c
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-09-19 18:18:20 +0530

    updated authentication component

--- src/Authentication/AuthForm.js
+++ src/Authentication/AuthForm.js
@@ -1,121 +1,154 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { authActions } from '../store/authReducer';
-import {useNavigate} from 'react-router-dom';
-import { useState, useRef } from 'react';
-import {Form, Container} from 'react-bootstrap';
+import React, { useState, useRef, useEffect } from "react";
 import classes from './AuthForm.module.css';
 import LoaderEl from '../UI/Loader/Loader';
+import { Form, Container } from 'react-bootstrap';
 
+import { useNavigate } from "react-router-dom";
 
+// import { Link } from "react-router-dom";
 
-const AuthForm = () => {
-    const emailInputRef = useRef();
-    const passwordInputRef = useRef();
-
-    const navigate = useNavigate();
-
-
-    // const authCtx = useContext(AuthContext)
-    const dispatch = useDispatch();
-    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-
+function AuthPage() {
     const [isLogin, setIsLogin] = useState(true);
+    const confPassRef = useRef();
+    const navigate = useNavigate()
+    const [isPassSame, setIsPassSame] = useState(true);
+    const [isError, setIsError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
+
+
+    const emailRef = useRef();
+    const passRef = useRef();
 
     const switchAuthModeHandler = () => {
         setIsLogin((prevState) => !prevState);
     };
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        const enteredEmail = emailInputRef.current.value;
-        const enteredPassword = passwordInputRef.current.value;
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        setIsPassSame(true);
+        setIsError(false);
 
-        setIsLoading(true);
-
-        let url;
+        if (passRef.current.value !== confPassRef.current.value) {
+            setIsPassSame(false);
+            return;
+        }
+        const email = emailRef.current.value;
+        const password = passRef.current.value;
 
         if (isLogin) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCc1DFQFUbZRDLki9bM5v-50uHdsd98I1w'
-        } else {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCc1DFQFUbZRDLki9bM5v-50uHdsd98I1w'
-        }
-        fetch(url,
-            {
-                method: 'POST',
-                body: JSON.stringify({
-                    email: enteredEmail,
-                    password: enteredPassword,
-                    returnSecureToken: true
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
+            try {
+                setIsLoading(true);
+                const response = await fetch(
+                    `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCc1DFQFUbZRDLki9bM5v-50uHdsd98I1w`,
+                    {
+                        method: "POST",
+                        body: JSON.stringify({
+                            email,
+                            password,
+                            returnSecureToken: true,
+                        }),
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }
+                );
+                setIsLoading(false);
+                console.log(response)
+                if (response.ok) {
+                    localStorage.setItem('token', response.idToken)
+                    navigate('/')
+
+                } else {
+                    setIsError('sorry ! something went wrong')
                 }
-            }
-        ).then(res => {
+                const data = await response.json();
 
-            setIsLoading(false);
+                localStorage.setItem("token", data.idToken);
+                localStorage.setItem("email", data.email);
+            } catch (err) {
+                setIsLoading(false);
+                setIsError(err);
 
-            if (res.ok) {
-                return res.json();
-            } else {
-                return res.json().then(data => {
-                    let errorMessage = 'Authentication Failed';
-                    throw new Error(errorMessage);
-                });
+
             }
-        }).then(data => {
-            dispatch(authActions.login());
-            console.log("this is data", data)
-            navigate('/profile')
-        }).catch(err => {
-            alert(err.message);
-        })
-    }
+        } else {
+            try {
+                setIsLoading(true);
+                console.log('email and password is ', email, password,)
+                const response = await fetch(
+                    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCc1DFQFUbZRDLki9bM5v-50uHdsd98I1w`,
+                    {
+                        method: "POST",
+                        body: JSON.stringify({
+                            email,
+                            password,
+                            returnSecureToken: true,
+                        }),
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    }
+                );
+                setIsLoading(false);
+
+                if (response.ok) {
+                    console.log("your authantication is successfull,", response);
+                }
+            } catch (err) {
+                setIsLoading(false);
+                setIsError(err);
+                throw new Error("your authantication faied because, ", err);
+            }
+        }
+    };
+
+
 
     return (
         <Container className={classes.auth}>
-            <Form onSubmit={submitHandler} className={classes.form} >
+            <Form onSubmit={submitHandler} className={classes.form}>
+                <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+                <Form.Group className={classes.control}>
+                    <Form.Label htmlFor="email">Your Email</Form.Label>
+                    <Form.Control type='email' id='email' required ref={emailRef} />
+                </Form.Group>
 
-                    <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-                    <Form.Group className={classes.control}>
-                        <Form.Label htmlFor='email'>Your Email</Form.Label>
-                        <Form.Control type='email' id='email' required ref={emailInputRef} />
-                    </Form.Group>
-                    <Form.Group className={classes.control}>
-                        <Form.Label htmlFor='password'>Your Password</Form.Label>
-                        <Form.Control
-                            type='password'
-                            id='password'
-                            required
-                            ref={passwordInputRef}
-                        />
-                    </Form.Group>
+                <Form.Group className={classes.control}>
+                    <Form.Label htmlFor='password'>Your Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        id='password'
+                        required
+                        ref={passRef}
+                    />
+                </Form.Group>
 
-
-
+                <Form.Group className={classes.control}>
+                    <Form.Label htmlFor="password">Confirm Password</Form.Label>
+                    <Form.Control as="input" type="password" id="password" ref={confPassRef} required />
+                </Form.Group>
+                
                 <div className={classes.actions}>
-                    {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
-                    {isLoading && <LoaderEl />}
-                    {!isLoggedIn && (<button
-                        type='button'
+                {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+                {isLoading && <LoaderEl />}
+                {!isPassSame && (
+                    <p className="text-danger">Sorry both Passwrods should same</p>
+                )}
+                {isError && <p className="text-danger">{isError}</p>}
+                    <button
+                        type="button"
                         className={classes.toggle}
                         onClick={switchAuthModeHandler}
                     >
-                        {isLogin ? 'Don"t have a account? Create one' : 'Already have an account? Login-In.'}
+                        {isLogin ? "Create new account" : "Login with existing account"}
                     </button>
-                    )}
-
-                    {isLoggedIn && (
-                        <button onClick={() => dispatch(authActions.logout())}>
-                            Logout
-                        </button>
-                    )}
                 </div>
+
             </Form>
         </Container>
     );
-};
+}
 
-export default AuthForm;
+export default AuthPage;
+{/* <button type="submit">{isLogin ? "Log In" : "Sign Up"}</button> */ }
